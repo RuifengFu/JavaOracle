af9b15467497e4d6d321e7fff02a48f190f281bf
RuifengFu <frf2000@tju.edu.cn>
2025-07-08 20:48:01 +0800

    chore: remove test files

--- src/test/java/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer();
-        sb.append((StringBuffer) null);
-        System.out.println(sb.toString()); // 输出"null"，而预期是抛出NullPointerException
-    }
-}
